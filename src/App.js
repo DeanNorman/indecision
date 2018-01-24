b000eb9f969e5340c23accbf945de9a269e5822f
Dean Norman <dean@22seven.com>
2018-01-24 17:05:34 +0200

    added basic outline

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react';
+// import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+// // class App extends Component {
+// //    constructor(props) {
+// //       super(props);
+// //       this.state = {
+// //          title: 'Indecision App',
+// //          subTitle: 'This is the subtitle',
+// //          term: '',
+// //          options: [],
+// //       };
+// //    }
+// //    render() {
+// //       const appData = {
+// //          title: 'Indecision App',
+// //          subTitle: 'This is the subtitle',
+// //          options: [],
+// //       }
 
-export default App;
+// //       const onFormSubmit = (e) => {
+         
+// //          e.preventDefault();
+// //          const option = e.target.elements.option.value;
+         
+// //          console.log(option);
+// //          if (option) {
+            
+// //             this.setState({
+// //                options: [option]
+// //             })
+// //             // appData.options.push(option);
+// //             // e.target.elements.option.value = '';  
+// //          }
+// //       }
+
+// //       const onRemoveAll = () => {
+// //          appData.options = [];
+// //       }
+
+// //       return (
+// //          <div className="App">
+// //             <header className="App-header">
+// //                <h1 className="App-title">{this.state.title}</h1>
+// //                <p>{this.state.options.length}</p>
+// //             </header>
+// //             <form onSubmit={onFormSubmit}>
+// //                <input
+// //                   // value ={ this.state.options }
+// //                   type="text"
+// //                   name="option" />
+// //                <button>Add Option</button>
+// //             </form>
+// //          </div>
+// //       );
+// //    }
+// // }
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,59 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+console.log('App.js is running!');
+
+const app = {
+   title: 'Indecision App',
+   subtitle: 'Put your life in the hands of a computer',
+   options: []
+};
+
+const onFormSubmit = (e) => {
+   e.preventDefault();
+
+   const option = e.target.elements.option.value;
+
+   if (option) {
+      app.options.push(option);
+      e.target.elements.option.value = '';
+      render();
+   }
+};
+
+const onRemoveAll = () => {
+   app.options = [];
+   render();
+};
+
+const appRoot = document.getElementById('app');
+
+const render = () => {
+   const template = (
+      <div>
+         <h1>{app.title}</h1>
+         {app.subtitle && <p>{app.subtitle}</p>}
+         <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+         <p>{app.options.length}</p>
+         <button onClick={onRemoveAll}>Remove All</button>
+         <ol>
+            {
+               app.options.map((option) => <li key={option}>{option}</li>)
+            }
+         </ol>
+         <form onSubmit={onFormSubmit}>
+            <input type="text" name="option" />
+            <button>Add Option</button>
+         </form>
+      </div>
+   );
+
+   ReactDOM.render(template, document.getElementById('root'));
+};
+
+render();
+
+
+// ReactDOM.render(template, document.getElementById('root'));
+// registerServiceWorker();
